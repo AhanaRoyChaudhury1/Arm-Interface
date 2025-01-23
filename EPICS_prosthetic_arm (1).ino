c4ced5a5f18526aae59534c0f11ab1528490d13e
mnovose <mnovose@purdue.edu>
2025-01-23 10:21:56 -0500

    Update EPICS_prosthetic_arm (1).ino

--- EPICS_prosthetic_arm (1).ino
+++ EPICS_prosthetic_arm (1).ino
@@ -28,7 +28,7 @@ void setup() {
 
   servo_wrist_x.attach(12);
   servo_wrist_y.attach(11);
-  servo_wrist_z.attach(10);
+  servo_wrist_twist.attach(10);
 
   servo_index.attach(3);
   servo_middle.attach(8);
@@ -78,11 +78,23 @@ void arm_extend_up(int degrees){
 // 180 to the right
 // 0 is palm down, 180 is palm up
 void wrist_twist_right(int degrees){
-
+   degrees += 5;
+    if (degrees > 180){
+      degrees = 180;
+    }
+  servo_wrist_twist(degrees);
+  delay(500); // half a second for the motion to be completed
+  return degrees;
 }
 
 void wrist_twist_left(int degrees){
-
+    degrees -= 5;
+    if (degrees < 0){
+      degrees = 0;
+    }
+    servo_wrist_twist(degrees);
+    delay(500); // half a second for the motion to be completed
+    return degrees;
 }
 
 void wrist_x(int degrees){
@@ -121,44 +133,84 @@ void loop() {
   int degrees_arm = 0;
   int degrees_wrist_x = 0;
   int degrees_wrist_y = 0;
-  int degrees_wrist_z = 0;
+  int degrees_wrist_twist = 0;
   int degrees_index = 0;
   int degrees_middle = 0;
   int degrees_ring = 0;
   int degrees_pinky = 0;
   int degrees_thumb = 0;
   
-
+  // arm
   while (voltage < .28){
     degrees_arm = arm_extend_up(degrees_arm);
   }
   while (voltage < .56 and voltage > .28){
     degrees_arm = arm_extend_down(degrees_arm);
   }
-  while (voltage < 1.12 and voltage > .56){
-    wrist_twist();
+
+  //wrist twisting
+  while (voltage < .84 and voltage > .56){
+    degrees_wrist_twist = wrist_twist_right(degrees_wrist_twist);
   }
-  while (voltage < 1.68 and voltage > 1.12 ){
-    wrist_x();
-  }
-  while (voltage < 2.24 and voltage > 1.68){
-    wrist_y();
-  }
-  while (voltage < 2.8 and voltage > 2.24){
-    index_finger();
-  }
-  while (voltage < 3.36 and voltage > 2.8){
-    middle_finger();
-  }
-  while (voltage < 3.92 and voltage > 3.36){
-    ring_finger();
-  }
-  while (voltage < 4.48 and voltage > 3.92){
-    pinky_finger();
-  }
-  while (voltage < 5 and voltage > 4.48){
-    thumb();
+  while (voltage < 1.12 and voltage > .84){
+    degrees_wrist_twist = wrist_twist_left(degrees_wrist_twist);
   }
 
+  //wrist left and right movement
+  while (voltage < 1.4 and voltage > 1.12 ){
+    degrees_wrist_x = wrist_x_right(degrees_wrist_x);
+  }
+  while (voltage < 1.68 and voltage > 1.4 ){
+    degrees_wrist_x = wrist_x_right(degrees_wrist_x);
+  }
+
+  //wrist up and down movement
+  while (voltage < 1.96 and voltage > 1.68){
+    degrees_wrist_y = wrist_y_up(degrees_wrist_y);
+  }
+  while (voltage < 2.24 and voltage > 1.96){
+    degrees_wrist_y = wrist_y_up(degrees_wrist_y);
+  }
+
+  // index finger movement 
+  while (voltage < 2.52 and voltage > 2.24){
+    degrees_index = index_finger_up(degrees_index);
+  }
+  while (voltage < 2.8 and voltage > 2.52){
+    degrees_index = index_finger_down(degrees_index);
+  }
+
+  // middle finger movement 
+  while (voltage < 3.08 and voltage > 2.8){
+    degrees_middle = middle_finger_up(degrees_middle);
+  }
+  while (voltage < 3.36 and voltage > 3.08){
+    degrees_middle = middle_finger_down(degrees_middle);
+  }
+
+  // ring finger movement 
+  while (voltage < 3.64 and voltage > 3.36){
+    degrees_ring = ring_finger_up(degrees_ring);
+  }
+  while (voltage < 3.92 and voltage > 3.64){
+    degrees_ring = ring_finger_down(degrees_ring);
+  }
+
+  // pinky finger movement 
+  while (voltage < 4.2 and voltage > 3.92){
+    degrees_pinky = pinky_finger_up(degrees_pinky);
+  }
+  while (voltage < 4.48 and voltage > 4.2){
+    degrees_pinky = pinky_finger_up(degrees_pinky);
+  }
+
+  // thumb movement 
+  while (voltage < 4.76 and voltage > 4.48){
+    degrees_thumb = thumb_up(degrees_thumb);
+  }
+
+  while (voltage < 5 and voltage > 4.76){
+    degrees_thumb = thumb_down (degrees_thumb);
+  }
 
 }
